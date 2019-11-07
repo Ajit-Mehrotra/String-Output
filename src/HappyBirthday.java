57c7a29d0a2353356d827f265a080a4e15a29b2c
Ajit_Mehrotra <ajitmehrotra2009@gmail.com>
2019-11-07 13:01:42 -0500

    First Commit

---
+++ src/HappyBirthday.java
@@ -0,0 +1,9 @@
+//A Program that prints  that prints out the sentence "Happy Birthday" to you four times on a separate line
+public class HappyBirthday {
+	public static void main(String args[]){
+		System.out.print("Happy Birthday"
+				+ "\nHappy Birthday"
+				+ "\nHappy Birthday"
+				+ "\nHappy Birthday");
+	}
+}
